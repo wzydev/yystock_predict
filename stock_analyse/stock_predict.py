478cdd23022b8f3fe18c5442e8daf9fd1254e9f3
wzydev <wzydev@163.com>
2017-09-23 23:20:01 +0800

    add predict

---
+++ stock_analyse/stock_predict.py
@@ -0,0 +1,99 @@
+#coding=utf-8
+'''
+
+@author: JayWong
+'''
+import pdb
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+from pymongo import MongoClient
+from lib_preprocess import *
+
+
+rnn_unit=10       #hidden layer units
+input_size=7
+output_size=1
+lr=0.0006
+
+
+client = MongoClient("s.izhiyi.pub",11281)
+coll = client["test"]["stock_history"]
+
+weights={
+         'in':tf.Variable(tf.random_normal([input_size,rnn_unit])),
+         'out':tf.Variable(tf.random_normal([rnn_unit,1]))
+        }
+biases={
+        'in':tf.Variable(tf.constant(0.1,shape=[rnn_unit,])),
+        'out':tf.Variable(tf.constant(0.1,shape=[1,]))
+       }
+
+def lstm(X):     
+    batch_size=tf.shape(X)[0]
+    time_step=tf.shape(X)[1]
+    w_in=weights['in']
+    b_in=biases['in']  
+    input=tf.reshape(X,[-1,input_size])
+    input_rnn=tf.matmul(input,w_in)+b_in
+    input_rnn=tf.reshape(input_rnn,[-1,time_step,rnn_unit])
+    cell=tf.nn.rnn_cell.BasicLSTMCell(rnn_unit)
+    init_state=cell.zero_state(batch_size,dtype=tf.float32)
+    output_rnn,final_states=tf.nn.dynamic_rnn(cell, input_rnn,initial_state=init_state, dtype=tf.float32)
+    output=tf.reshape(output_rnn,[-1,rnn_unit])
+    w_out=weights['out']
+    b_out=biases['out']
+    pred=tf.matmul(output,w_out)+b_out
+    return pred,final_states
+
+
+
+def train_lstm(coll,scode,batch_size=60,time_step=20):
+    X=tf.placeholder(tf.float32, shape=[None,time_step,input_size])
+    Y=tf.placeholder(tf.float32, shape=[None,time_step,output_size])
+    batch_index,train_x,train_y=get_train_data_tmw_low(coll,scode)
+    pred,_=lstm(X)
+    loss=tf.reduce_mean(tf.square(tf.reshape(pred,[-1])-tf.reshape(Y, [-1])))
+    train_op=tf.train.AdamOptimizer(lr).minimize(loss)
+    saver=tf.train.Saver(tf.global_variables(),max_to_keep=15)
+    module_file = tf.train.latest_checkpoint("model/")    
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        #saver.restore(sess, module_file)
+        for i in range(2000):
+            for step in range(len(batch_index)-1):
+                _,loss_=sess.run([train_op,loss],feed_dict={X:train_x[batch_index[step]:batch_index[step+1]],Y:train_y[batch_index[step]:batch_index[step+1]]})
+            print(i,loss_)
+            if i % 200==0:
+                print("save modelº",saver.save(sess,'model/stock2.model',global_step=i))
+
+
+#train_lstm(coll,"600452")
+
+
+def prediction(coll,scode,time_step=20):
+    X=tf.placeholder(tf.float32, shape=[None,time_step,input_size])
+    #Y=tf.placeholder(tf.float32, shape=[None,time_step,output_size])
+    mean,std,test_x,test_y=get_predict_data(coll,scode)
+    pred,_=lstm(X)     
+    saver=tf.train.Saver(tf.global_variables())
+    with tf.Session() as sess:
+        module_file = tf.train.latest_checkpoint("model/")
+        saver.restore(sess, module_file) 
+        test_predict=[]
+        pdb.set_trace()
+        for step in range(len(test_x)):
+          prob=sess.run(pred,feed_dict={X:[test_x[step]]})   
+          predict=prob.reshape((-1))
+          test_predict.extend(predict)
+        test_y=np.array(test_y)*std[7]+mean[7]
+        test_predict=np.array(test_predict)*std[7]+mean[7]
+        acc=np.average(np.abs(test_predict-test_y[:len(test_predict)])/test_y[:len(test_predict)])
+        plt.figure()
+        plt.plot(list(range(len(test_predict))), test_predict, color='b')
+        plt.plot(list(range(len(test_y))), test_y,  color='r')
+        plt.show()
+
+prediction(coll,"600452") 
